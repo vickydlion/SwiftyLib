30782268acf355b26731172ce669b5cebfa525eb
vivek <vivek@mishipay.com>
2021-06-23 15:50:54 +0530

    Initial project setup

---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Vivek Garg on 23/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
